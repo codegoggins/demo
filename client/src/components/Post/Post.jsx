01709bfc0b58cd6ee1cb799009627aab4336e44e
Nilay Singh <snilay011@gmail.com>
2022-11-09 09:30:26 +0530

    Single Post Done

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -4,14 +4,26 @@ import './Post.css'
 const Post = () => {
   return (
     <div className='post'>
-         <img src="https://4.imimg.com/data4/VK/VX/MY-3291959/r-d-sharma-maths-for-12th-std-cbse-500x500.jpg" alt="" srcset="" />
+        <div className='post-container'>
+        <div>
+        <img src="https://m.media-amazon.com/images/I/71MjzETlTOL.jpg" alt="" srcset="" />
+        </div>
          <div className='post-content'>
+              <div className='book-details'>
               <h4 className='item-name'>RD SHARMA</h4>
               <p className='item-desc'>Mathematics Books</p>
-              <h5>Price :Rs. 600</h5>
-              <h5>Seller: Nilay Singh</h5>
-              <p>Uploaded 1 hr ago</p>
+              </div>
+              <div className='user-details'>
+                <div className='user'>
+                  <p>Price : Rs. 600</p>
+                  <p>Seller: Nilay Singh</p>
+                </div>
+                <div className='upload-time'>
+                  <p>1 hr ago</p>
+                </div>
+              </div>
          </div>
+        </div>
     </div>
   )
 }
