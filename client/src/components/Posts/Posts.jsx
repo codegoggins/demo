eba37c0bbe79f8f5f3a0ff88656bf22b268353bb
Nilay Singh <snilay011@gmail.com>
2022-11-11 19:49:39 +0530

    Posts Section Updated

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -5,14 +5,18 @@ import './Posts.css'
 const Posts = () => {
   return (
     <div className='posts'>
-         <div className='posts-container'>
-              <h1 className='posts-title'>Items on Sale</h1>
-              <div className='posts-items'>
-                   <div className='post'>
                         <Post/>
-                   </div>
-              </div>
-         </div>
+                        <Post/>
+                        <Post/>
+                        <Post/>
+                        <Post/>
+                        <Post/> 
+                        <Post/>
+                        <Post/>
+                        <Post/>
+                        <Post/>
+                        <Post/>
+                        <Post/> 
     </div>
   )
 }
