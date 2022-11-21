e6b844dfe25bfe20ec699e580e571a888d3b7aed
Nilay Singh <snilay011@gmail.com>
2022-11-22 02:10:09 +0530

    Single Post Page Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar/Navbar";
 import Posts from "./components/Posts/Posts";
 import Home from "./pages/Home/Home";
+import Single from "./pages/Single/Single";
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
-        <Home/>
-        <Posts/>
+        {/* <Home/>
+        <Posts/> */}
+        <Single/>
     </div>
   );
 }
---
+++ client/src/pages/Single/Single.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SinglePost from '../../components/Single Post/SinglePost'
+import './Single.css'
+
+const Single = () => {
+  return (
+    <div className='single'>
+      <SinglePost/>
+    </div>
+  )
+}
+
+export default Single
