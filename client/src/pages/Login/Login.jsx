e0d809f07f632e76f551ec42ef930ca9c90da20b
Nilay Singh <snilay011@gmail.com>
2022-11-24 00:30:59 +0530

    Login Form CSS Done

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
     <div className='login'>
-        <div className="container">
-            <form className='loginForm'>
+        <div className="login-container">
+            <div className='login-img'>
+            <img src="https://images.pexels.com/photos/721287/pexels-photo-721287.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+            </div>
+            <div className='login-form'>
+            <span>Sign In</span>
+            <form>
                 <label>Username</label>
                 <input type="text" placeholder='Enter username' />
                 <label>Password</label>
                 <input type="text" placeholder='Enter password' />
                 <button className='loginButton'>Login</button>
+                <button className='registerButton'>Register</button>
             </form>
+            </div>
         </div>
     </div>
   )
